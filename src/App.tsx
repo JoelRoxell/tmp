b35b404f4bc88b9ca555eb337d9f6ea74e512df0
noone <joel.roxell@annevo.se>
2018-12-13 09:37:30 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import style from "./App.module.scss";
+
+class App extends Component {
+  render() {
+    console.log(style);
+
+    return (
+      <div className={style.App}>
+        <header className={style.AppHeader}>
+          <img src={logo} className={style.AppLogo} alt="logo" />
+
+          <p>
+            Edit <code>src/App.js</code> and save to reload...
+          </p>
+
+          <a
+            className={style.AppLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
